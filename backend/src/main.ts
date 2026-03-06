7e10a7fda96b92c39fd469f9946fd5098ff9c908
Doos <a8a8a30@gmail.com>
2026-03-06 19:17:02 +0300

    Initial commit of express server with Socket.io functionality

---
+++ backend/src/main.ts
@@ -0,0 +1,26 @@
+// Express server with Socket.io
+
+import express from 'express';
+import http from 'http';
+import { Server } from 'socket.io';
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+    res.send('Car Rental Management API');
+});
+
+io.on('connection', (socket) => {
+    console.log('A user connected');
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
